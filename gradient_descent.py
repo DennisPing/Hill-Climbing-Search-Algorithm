551d8878d2206c5cb0d0498f1726a509f0c05982
DennisPing <idennisping@gmail.com>
2021-06-13 17:22:44 -0400

    Moved all repeated functions into one tsp script

--- gradient_descent.py
+++ gradient_descent.py
@@ -1,134 +1,55 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
 import sys
-from numba import njit, types, typed
 from time import time
 import numpy as np
-from geopy.distance import distance
+from numba import njit
 from matplotlib import pyplot as plt
 from io_manager import IOManager
-import itertools
-from random import randint
-
-class Parser:
-    """
-    A class to parse the input file. Remove the first line.
-    Reformats cities into tuples of (cityname, longititude, latitude)
-    """
-    def __init__(self):
-        pass
-
-    def read_file(self, filepath):
-        with open(filepath) as data:
-
-            cities = []
-            lines = data.readlines()[1:]
-            for line in lines:
-                line = line.rstrip()
-                arr = line.split(',')
-                cities.append((arr[0], arr[1], arr[2]))
-            return cities
+from tsp import build_cities_index, build_distance_map, random_solution, total_distance
 
 @njit
-def random_solution(citiesIdx):
-    """
-    Generate a random solution from a list of city indexes.
-    :param citiesIdx: A list of city indexes.
-    :return: A list of city indexes in random order for the Traveling Salesman to traverse.
-    """
-    solution = citiesIdx.copy() # Avoid nasty bug with object reference when you shuffle
-    np.random.shuffle(solution)
-    return solution
-
-def build_cities_index(cities):
-    """
-    Index the cities into a numpy array so we can refer to cities as integers.
-    :param cities: A list of city tuples composed of (cityname, longitude, latitude).
-    :return: A list of cities labeled as integers.
-    """
-    citiesIdx = np.arange(0, len(cities))
-    return citiesIdx
-
-def build_distance_map_redux(cities, citiesIdx):
-    """
-    Calculate all pairwise distances between cities and put them in a dictionary.
-    Run this method one time before doing hill climbing iterations.
-    :param cities: A list of city tuples composed of (cityname, longitude, latitude).
-    :param citiesIdx: A list of cities labeled as integers.
-    :return: A dictionary of all pairwise distances between cities.
-    """
-    citiesMap = {}
-    for i, idxNum in enumerate(citiesIdx):
-        citiesMap[idxNum] = cities[i]
-
-    distanceMap = typed.Dict.empty(key_type=types.UniTuple(types.int64, 2), value_type=types.int64)
-    # Note: permutations take up 2x more memory, but the eventual lookup is 2x faster.
-    # This is because you don't need to stop, switch the tuple, and then lookup again.
-    allPermutations = itertools.permutations(citiesIdx, 2)
-    for i, each in enumerate(allPermutations):
-        cityA = each[0] # These are the numeric values of cities
-        cityB = each[1]
-
-        coordA = (citiesMap[cityA][2], citiesMap[cityA][1]) # Get the city tuple from temp map
-        coordB = (citiesMap[cityB][2], citiesMap[cityB][1])
-        dist = distance(coordA, coordB).km
-        distanceMap[each] = int(dist)
-    print("SUCCESSFUL DISTANCE MAP BUILD")
-    return distanceMap, citiesMap
-
-@njit
-def total_distance(solution, distanceMap):
-    """
-    Calculate the total distance among a solution of cities.
-    Uses a dictionary to get lookup the each pairwise distance.
-    :param solution: A list of cities in random order.
-    :distanceMap: The dictionary lookup tool.
-    :return: The total distance between all the cities.
-    """
-    totalDistance = 0
-    for i, city in enumerate(solution[:-1]): # Stop at the second to last city.
-        cityA = city
-        cityB = solution[i+1]
-        buildKey = (cityA, cityB)
-        totalDistance += distanceMap[buildKey]
-    # Build the key to return home
-    cityA = solution[-1]
-    cityB = solution[0]
-    goHome = (cityA, cityB)
-    totalDistance += distanceMap[goHome]
-    return totalDistance
-
-@njit
-def find_best_neighbor(solution, distanceMap):
+def find_best_neighbor(solution, distanceMap, limit=None):
     """
     Create all possible neighbors by switching 1 pair of cities.
     Also checks to see if this new distance is better than the current distance.
     :param solution: A list of cities in random order.
     :param distanceMap: The dictionary lookup tool required to pass into total_distance function.
+    :param limit: The number of times to swap cities.
     :return: The best neighbor and best distance out of all possible neighbors.
     """
     bestDist = sys.maxsize
     bestNeighbor = None
-    # for i in range(len(solution)):
-    #     for j in range(i + 1, len(solution)):
-    #         neighbor = solution.copy()
-    #         neighbor[i], neighbor[j] = solution[j], solution[i]
-    #         dist = total_distance(neighbor, distanceMap)
-    #         if dist < bestDist:
-    #             bestDist = dist
-    #             bestNeighbor = neighbor
-    count = 0
-    while count < 500:
-        j = randint(0, len(solution)-1)
-        k = randint(1, len(solution)-1)
-        if j == k:
-            k = randint(1, len(solution)-1)
-        neighbor = solution.copy()
-        neighbor[j], neighbor[k] = solution[k], solution[j]
-        dist = total_distance(neighbor, distanceMap)
-        if dist < bestDist:
-            bestDist = dist
-            bestNeighbor = neighbor
-        count += 1
-    return bestNeighbor, bestDist
+
+    # No limit, find every neighbor by swapping two cities.
+    if limit is None:
+        for i in range(len(solution)):
+            for j in range(i + 1, len(solution)):
+                neighbor = solution.copy()
+                neighbor[i], neighbor[j] = solution[j], solution[i]
+                dist = total_distance(neighbor, distanceMap)
+                if dist < bestDist:
+                    bestDist = dist
+                    bestNeighbor = neighbor
+        return bestNeighbor, bestDist
+
+    # Set a limit of swaps.
+    else:
+        count = 0
+        while count < limit:
+            # Make a pair of unique, random numbers
+            pickTwo = np.random.choice((len(solution)-1), 2, replace=False)
+            j = int(pickTwo[0])
+            k = int(pickTwo[1])
+            neighbor = solution.copy()
+            neighbor[j], neighbor[k] = solution[k], solution[j]
+            dist = total_distance(neighbor, distanceMap)
+            if dist < bestDist:
+                bestDist = dist
+                bestNeighbor = neighbor
+            count += 1
+        return bestNeighbor, bestDist
 
 def main():
     """
@@ -137,15 +58,13 @@ def main():
     """
     ioManager = IOManager()
     filename, iterations, rounds = ioManager.prompt_input()
-
-    parser = Parser()
-    cities = parser.read_file(filename)
+    cities = ioManager.read_file(filename)
     
     print("\nStarting gradient descent search")
     startTime = time()
     citiesIdx = build_cities_index(cities)
 
-    distanceMap, citiesMap = build_distance_map_redux(cities, citiesIdx)
+    distanceMap, citiesMap = build_distance_map(cities, citiesIdx)
 
     plt.style.use("ggplot")
     fig, ax = plt.subplots(figsize=(8,6))
@@ -160,7 +79,7 @@ def main():
         bestSolution = None
         for i in range(iterations):
             solution = random_solution(citiesIdx)
-            bestNeighbor, shortestDist = find_best_neighbor(solution, distanceMap)
+            bestNeighbor, shortestDist = find_best_neighbor(solution, distanceMap, limit=1000)
             if shortestDist < bestDist:
                 bestDist = shortestDist
                 bestSolution = bestNeighbor
--- hill_climbing.py
+++ hill_climbing.py
@@ -1,101 +1,13 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
 import sys
-import os
-from numba import njit, types, typed
 from time import time
 import numpy as np
-from geopy.distance import distance
+from numba import njit
 from matplotlib import pyplot as plt
 from io_manager import IOManager
-import itertools
-
-class Parser:
-    """
-    A class to parse the input file. Remove the first line.
-    Reformats cities into tuples of (cityname, longititude, latitude)
-    """
-    def __init__(self):
-        pass
-
-    def read_file(self, filepath):
-        with open(filepath) as data:
-
-            cities = []
-            lines = data.readlines()[1:]
-            for line in lines:
-                line = line.rstrip()
-                arr = line.split(',')
-                cities.append((arr[0], arr[1], arr[2]))
-            return cities
-
-@njit
-def random_solution(citiesIdx):
-    """
-    Generate a random solution from a list of city indexes.
-    The home city is automatically added to the end of the list.
-    :param citiesIdx: A list of city indexes.
-    :return: A list of city indexes in random order for the Traveling Salesman to traverse.
-    """
-    solution = citiesIdx.copy() # Avoid nasty bug with object reference when you shuffle
-    np.random.shuffle(solution)
-    return solution
-
-def build_cities_index(cities):
-    """
-    Index the cities into a numpy array so we can refer to cities as integers.
-    :param cities: A list of city tuples composed of (cityname, longitude, latitude).
-    :return: A list of cities labeled as integers.
-    """
-    citiesIdx = np.arange(0, len(cities))
-    return citiesIdx # Initial build does not include return home!
-
-def build_distance_map_redux(cities, citiesIdx):
-    """
-    Calculate all pairwise distances between cities and put them in a dictionary.
-    Run this method one time before doing hill climbing iterations.
-    :param cities: A list of city tuples composed of (cityname, longitude, latitude).
-    :param citiesIdx: A list of cities labeled as integers.
-    :return: A dictionary of all pairwise distances between cities.
-    """
-    citiesMap = {}
-    for i, idxNum in enumerate(citiesIdx):
-        citiesMap[idxNum] = cities[i]
-
-    distanceMap = typed.Dict.empty(key_type=types.UniTuple(types.int64, 2), value_type=types.int64)
-    # Note: permutations take up 2x more memory, but the eventual lookup is 2x faster.
-    # This is because you don't need to stop, switch the tuple, and then lookup again.
-    allPermutations = itertools.permutations(citiesIdx, 2)
-    for i, each in enumerate(allPermutations):
-        cityA = each[0] # These are the numeric values of cities
-        cityB = each[1]
-
-        coordA = (citiesMap[cityA][2], citiesMap[cityA][1]) # Get the city tuple from temp map
-        coordB = (citiesMap[cityB][2], citiesMap[cityB][1])
-        dist = distance(coordA, coordB).km
-        distanceMap[each] = int(dist)
-    print("SUCCESSFUL DISTANCE MAP BUILD")
-    return distanceMap, citiesMap
-
-@njit
-def total_distance(solution, distanceMap):
-    """
-    Calculate the total distance among a solution of cities.
-    Uses a dictionary to get lookup the each pairwise distance.
-    :param solution: A list of cities in random order.
-    :distanceMap: The dictionary lookup tool.
-    :return: The total distance between all the cities.
-    """
-    totalDistance = 0
-    for i, city in enumerate(solution[:-1]): # Stop at the second to last city.
-        cityA = city
-        cityB = solution[i+1]
-        buildKey = (cityA, cityB)
-        totalDistance += distanceMap[buildKey]
-    # Build the key to return home
-    cityA = solution[-1]
-    cityB = solution[0]
-    goHome = (cityA, cityB)
-    totalDistance += distanceMap[goHome]
-    return totalDistance
+from tsp import build_cities_index, build_distance_map, random_solution, total_distance
 
 @njit
 def find_best_neighbor(solution, distanceMap):
@@ -123,14 +35,12 @@ def main():
     """
     ioManager = IOManager()
     filename, iterations, rounds = ioManager.prompt_input()
-
-    parser = Parser()
-    cities = parser.read_file(filename)
+    cities = ioManager.read_file(filename)
     
     print("\nStarting simple hill climbing search")
     startTime = time()
     citiesIdx = build_cities_index(cities)
-    distanceMap, citiesMap = build_distance_map_redux(cities, citiesIdx)
+    distanceMap, citiesMap = build_distance_map(cities, citiesIdx)
 
     plt.style.use("ggplot")
     fig, ax = plt.subplots(figsize=(8,6))

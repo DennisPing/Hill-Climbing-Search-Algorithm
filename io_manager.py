4dd05789651edea7a537e756c0007cc8fb04267c
DennisPing <idennisping@gmail.com>
2021-06-13 17:20:49 -0400

    Refactor all I/O file operations into one class

--- io_manager.py
+++ io_manager.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
 import os
 import sys
 
@@ -80,6 +83,21 @@ class IOManager:
                 print(f"Error, '{numRounds}' must be a positive integer.")
                 continue
     
+    def read_file(self, filepath):
+        """
+        Read the input file. Remove the first line.
+        :param filepath: The input file to read
+        :return: A list of cities as tuples of (cityname, longititude, latitude)
+        """
+        with open(filepath) as data:
+            cities = []
+            lines = data.readlines()[1:]
+            for line in lines:
+                line = line.rstrip()
+                arr = line.split(',')
+                cities.append((arr[0], arr[1], arr[2]))
+            return cities
+    
     def write_file(self, algo, filename, absBestDistance, absBestSolution, citiesMap):
         """
         Write the absolute best solution to disk.
@@ -94,7 +112,10 @@ class IOManager:
         baseName = tail.split('.')[0] # split the period and get the base name
         outfile = open(f"{self.cwd}/{algo}_{baseName}_best_solution.txt", mode='w')
         outfile.write(f"Shortest path found: {absBestDistance} km\n")
-        for eachCity in absBestSolution:
+        for eachCity in absBestSolution[:-1]:
             cityTuple = citiesMap[int(eachCity)]
-            outfile.write(str(cityTuple) + '\n')
+            outfile.writelines(','.join(map(str, cityTuple)))
+            outfile.write('\n')
+        cityTuple = citiesMap[int(absBestSolution[-1])]
+        outfile.writelines(','.join(map(str, cityTuple))) # Don't write a trailing new line
         outfile.close()
